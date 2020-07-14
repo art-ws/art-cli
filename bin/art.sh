80bc65b00bcd41addae888c9390dcf43b3315e8e
Alexander Shagin <alexander.shagin@aristsoft.com>
2020-07-14 14:57:21 +0000

    "refactor: env names"

--- bin/art.sh
+++ bin/art.sh
@@ -5,38 +5,50 @@ die() {
   exit 1
 }
 
+check_dir(){
+  [ ! -d $1 ] && die "Required directory $1 not exists"
+  return 0
+}
+
+check_file(){
+  [ ! -f $1 ] && die "Required file $1 not exists"
+  return 0
+}
+
+check_var(){
+  [ -z $1 ] && die "Variable name not specified"
+  local varname=$1
+  # https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html
+  [ -z  ${!varname} ] && die "Required variable '$varname' not defined"  
+  return 0
+}
+
 # try to load ~/.artrc
 [ -f ~/.artrc ] && source ~/.artrc
 
 # define core env veriables
 [ -z $ART_USER_NAME  ] && ART_USER_NAME="$USER"
 [ -z $ART_USER_EMAIL ] && ART_USER_EMAIL="${ART_USER_NAME}@`hostname`"
+
 [ -z $ART_ROOT ] && ART_ROOT=/opt/art
 [ -z $ART_REPO_ROOT ] && ART_REPO_ROOT=$ART_ROOT/p
+ARTCLI_HOME=$ART_REPO_ROOT/art-cli
 
-[ ! -d $ART_REPO_ROOT ] && die "Directory '$ART_REPO_ROOT' not exists"
+check_dir $ART_REPO_ROOT
+check_dir $ARTCLI_HOME
 
-ARTCLI_HOME=$ART_ROOT/art-cli
-source $ARTCLI_HOME/bin/ENV
-
-dump_args(){
- local n=0
- while [ "$1" ]
- do
-  ((n+=1))
-  echo "$n = [$1]"
-  shift
- done
-}
+check_file $ARTCLI_HOME/bin/consts && source $ARTCLI_HOME/bin/consts 
 
 exec_project_dir(){
   local exec_dir=$1
+  check_var exec_dir
   shift
 
-  [ -z $ART_PROJECT_PATH ] && die "ART_PROJECT_PATH not defined"
+  check_var ARTCLI_PROJECT_PATH
+    
+  local cwd="$ARTCLI_PROJECT_PATH"
+  check_dir $cwd
 
-
-  local cwd="$ART_PROJECT_PATH"
   local project_path=`dirname $cwd`
   local project_name=`basename $project_path`
 
@@ -46,21 +58,26 @@ exec_project_dir(){
   [ "$exec_dir" != "." ] && prefix="$exec_dir/"
 
   echo -e "\E[1;34m[$project_name]"
-  local about_file="$full_path/$ART_CLI_ABOUT_DIR_FILE"
+
+  check_var ARTCLI_CONST_ABOUT_FILE
+
+  local about_file="$full_path/$ARTCLI_CONST_ABOUT_FILE"
   if [ -f "$about_file" ]; then
     echo -e "\E[0;37m`cat $about_file`"
   fi
+
+  check_var ARTCLI_CONST_EXEC
 
   local n=0
   for f in `ls $full_path | grep -v "^_"`
   do
     n=$((n+1))
     if [ -f $full_path/$f ] ; then
-      echo -e "\E[1;39m$n) . $ART_CLI_CMD ${prefix}${f}"
+      echo -e "\E[1;39m$n) $ARTCLI_CONST_EXEC ${prefix}${f}"
     fi
     if [ -d $full_path/$f ] ; then
       local num=`find $full_path/$f -type f | wc -l`
-      echo -e "\E[1;33m$n) * $ART_CLI_CMD ${prefix}${f} - $num command(s)"
+      echo -e "\E[1;33m$n) * $ARTCLI_CONST_EXEC ${prefix}${f} - $num command(s)"
     fi
   done
   echo -e "\E[0;37m"
@@ -68,22 +85,24 @@ exec_project_dir(){
 
 exec_project_file(){
   local exec_file=$1
+  check_var exec_file
   shift
 
-  [ -z $ART_PROJECT_PATH ] && die "ART_PROJECT_PATH not defined"
+  check_var ARTCLI_PROJECT_PATH
 
-  local full_path="$ART_PROJECT_PATH/$exec_file"
+  local full_path="$ARTCLI_PROJECT_PATH/$exec_file"
 
-  ART_EXEC_FILE_DIR=`dirname $full_path`
-  ART_EXEC_FILE_NAME=`basename $full_path`
+  ARTCLI_ACTION_DIR_PATH=`dirname $full_path`
+  ARTCLI_ACTION_FILE_NAME=`basename $full_path`
   
-  ART__DIRNAME=$ART_EXEC_FILE_DIR
-  ART__BASENAME=$ART_EXEC_FILE_NAME
+  check_var ARTCLI_ACTION_DIR_PATH
+  check_var ARTCLI_ACTION_FILE_NAME
+  check_var ARTCLI_CONST_BEFORE_FILE
 
-  local before_file="$ART_EXEC_FILE_DIR/$ART_CLI_EXEC_BEFORE"
+  local before_file="$ARTCLI_ACTION_DIR_PATH/$ARTCLI_CONST_BEFORE_FILE"
   [ -f "$before_file" ] && source $before_file
 
-  local help_file=$ART_EXEC_FILE_DIR/"_$ART_EXEC_FILE_NAME.txt"
+  local help_file=$ARTCLI_ACTION_DIR_PATH/"_$ARTCLI_ACTION_FILE_NAME.txt"
   if [ -f $help_file ] && [ "$1" == "help" ];
   then
     cat $help_file
@@ -97,55 +116,58 @@ exec_project_file(){
 }
 
 exec_project(){
- local project=$1
- shift
- [ -z $project ] && die "project not defined"
-
- local project_root_path="$ART_ROOT/$project"
- local project_path="$project_root_path/$ART_CLI_DIR"
- local path=$1
- shift
- [ -z $path ] && die "path not defined"
-
- local full_path="$project_path/$path"
-
- ART_PROJECT_ROOT_PATH="$project_root_path"
- ART_PROJECT="$project"
- ART_PROJECT_PATH="$project_path"
- ART_EXEC_ACTION="$path"
-
- if [ -f $full_path ]; then
-   exec_project_file "$path" "$@"
-   return $?
- elif [ -d $full_path ]; then
-   exec_project_dir "$path" "$@"
-   return $?
- fi
-
- return 1
-}
-
-execute_from_project(){
   local project=$1
+  check_var project
   shift
-  local path=$1
-  shift
-  [ -z $project ] && die "project not defined"
-  [ -z $path ] && die "path not defined"
 
+  check_dir $ART_REPO_ROOT
+  check_var ARTCLI_CONST_DIR
+
+  local project_root_path="$ART_REPO_ROOT/$project"
+  local project_path="$project_root_path/$ARTCLI_CONST_DIR"
+  
+  local path=$1
+  check_var path
+  shift
+
+  local full_path="$project_path/$path"
+
+  ARTCLI_PROJECT_ROOT="$project_root_path"
+  ARTCLI_PROJECT="$project"
+  ARTCLI_PROJECT_PATH="$project_path"
+  ARTCLI_ACTION="$path"
+
+  check_var ARTCLI_PROJECT_ROOT
+  check_var ARTCLI_PROJECT
+  check_var ARTCLI_PROJECT_PATH
+  check_var ARTCLI_ACTION
+
+  if [ -f $full_path ]; then
+    exec_project_file "$path" "$@"
+    return $?
+  elif [ -d $full_path ]; then
+    exec_project_dir "$path" "$@"
+    return $?
+  fi
+
+  return 1
 }
 
 list_projects(){
-  ls -1 $ART_ROOT
+  check_dir $ART_REPO_ROOT
+  ls -1 $ART_REPO_ROOT
 }
 
 lookup_path_at_project(){
   local project=$1
   local path=$2
-  [ -z $project ] && die "project not defined"
-  [ -z $path ] && die "path not defined"
 
-  local full_path="$ART_ROOT/$project/$ART_CLI_DIR/$path"
+  check_var project
+  check_var path
+  check_dir $ART_REPO_ROOT
+  check_var ARTCLI_CONST_DIR
+
+  local full_path="$ART_REPO_ROOT/$project/$ARTCLI_CONST_DIR/$path"
   # http://ryanstutorials.net/bash-scripting-tutorial/bash-if-statements.php
   if [ -f "$full_path" ] || [ -d "$full_path" ]
   then
@@ -158,7 +180,8 @@ lookup_path_at_project(){
 
 lookup_path(){
   local path=$1
-  [ -z $path ] && die "path not defined"
+  check_var path
+
   for p in `list_projects`
   do
     full_path=`lookup_path_at_project $p $path`
@@ -172,13 +195,13 @@ lookup_path(){
 }
 
 lookup_project(){
- local path=$1
-  [ -z $path ] && die "path not defined"
+  local path=$1
+  check_var path
+  
   for p in `list_projects`
   do
     full_path=`lookup_path_at_project $p $path`
-    if [ $? -eq 0 ] && [ ! -z "$full_path" ]
-    then
+    if [ $? -eq 0 ] && [ ! -z "$full_path" ]; then
       echo "$p"
       return 0
     fi
@@ -188,8 +211,9 @@ lookup_project(){
 
 execute(){
   local path=$1
+  check_var path
   shift
-  [ -z $path ] && die "path not defined"
+  
   full_path=`lookup_path $path`
   if [ $? -eq 0 ] && [ ! -z "$full_path" ]
   then
@@ -200,17 +224,15 @@ execute(){
 }
 
 try_execute(){
- log "try_execute()" 
- local action=$1
- shift
- project=`lookup_project $action`
- [ $? -eq 0 ] || die "Action '$action' not found"
- log "project = $project"
- exec_project "$project" "$action" "$@"
+  local action=$1
+  check_var action
+  shift
+  local project=`lookup_project $action`
+  [ $? -eq 0 ] || die "Action '$action' not found"
+  exec_project "$project" "$action" "$@"
 }
 
 dump_all_commands(){
-  log "dump_all_commands()"
   for p in `list_projects`
   do
     exec_project $p "."
@@ -222,9 +244,7 @@ run(){
  local action=$1
  shift
 
- [ $debug == "1" ] && echo "TRY EXECUTE ACTION: $action"
- if [ -z $action ]
- then
+ if [ -z $action ]; then
    dump_all_commands
  else
    try_execute $action "$@"
